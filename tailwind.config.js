ebc6b959901b140f66a897e2a88000be19a180cc
dippal513 <dip.pal.513@gmail.com>
2023-12-22 10:19:45 +0600

    tailwind config solved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,26 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode:"class",
+  darkMode: 'class',
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-}
+  purge: {
+    content: [
+      './src/**/*.{js,ts,jsx,tsx}',
+    ],
+    options: {
+      safelist: [
+        // Add any specific classes you want to ensure are retained
+        'bg-gradient-radial',
+      ],
+    },
+  },
+};
